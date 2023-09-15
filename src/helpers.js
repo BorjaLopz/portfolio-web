d2f3ad134ae4dbf42086e397f06b2ed317ca34c1
Borja <lopzdiazborja@gmail.com>
2023-09-15 13:51:00 +0200

    Mostramos videos. TODO -> Meter portadas a los videos

---
+++ src/helpers.js
@@ -0,0 +1,12 @@
+// DICCIONARIO DE ESTADOS DE UN SERVICIO //
+const TYPE_OF_FILE = Object.freeze({
+  IMAGE: "IMAGE",
+  VIDEO: "VIDEO",
+});
+
+//Obtener la key de un diccionario por su value
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key] === value);
+}
+
+export { TYPE_OF_FILE, getKeyByValue };
